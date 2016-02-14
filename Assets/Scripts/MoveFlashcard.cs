343e20e4f7d460766d6de61ceb9ebf189383500a
Cole Marlar <jmarlar@Coles-MacBook-Air.local>
2016-02-14 15:36:37 -0600

    Added a fix for Note 4, added all the cards for study as well as a tutorial section to get used to the VR

--- Assets/Scripts/MoveFlashcard.cs
+++ Assets/Scripts/MoveFlashcard.cs
@@ -5,10 +5,13 @@ public class MoveFlashcard : MonoBehaviour {
 
 	public Texture[] cardsFront;
 	public Texture[] cardsBack;
+	public Texture[] sampleFront;
+	public Texture[] sampleBack;
     public Animator anim;
 	public Renderer rend;
 	int index = 0;
 	bool lookingAtBack = false;
+	bool tutorialMode = true;
 
 	// Use this for initialization
 	void Start () {
@@ -19,16 +22,36 @@ public class MoveFlashcard : MonoBehaviour {
 	// Update is called once per frame
 	bool isPressed = false;
 	void Update () {
+		// tutorial
+		if (Input.GetAxis ("Horizontal") > 0 && tutorialMode == true) {
+			tutorialMode = false;
+			index = 0;
+			rend.material.mainTexture = cardsFront [index];
+		}
+
 		if(!isPressed){
 			if(!lookingAtBack){
 				if(Input.GetAxis("Vertical") > 0){
-					index = (index + 1) % cardsFront.Length;
-					rend.material.mainTexture = cardsFront[index];
+					if (tutorialMode) {
+						index = (index + 1) % sampleFront.Length;
+						rend.material.mainTexture = sampleFront[index];
+					} else {
+						index = (index + 1) % cardsFront.Length;
+						rend.material.mainTexture = cardsFront[index];
+					}
+					// fade texture
 					isPressed = true;
 				}
 				else if(Input.GetAxis("Vertical") < 0){
-					index = (index + cardsFront.Length - 1) % cardsFront.Length;
-					rend.material.mainTexture = cardsFront[index];
+					if (tutorialMode) {
+						index = (index + sampleFront.Length - 1) % sampleFront.Length;
+						rend.material.mainTexture = sampleFront[index];
+					} else {
+						index = (index + cardsFront.Length - 1) % cardsFront.Length;
+						rend.material.mainTexture = cardsFront[index];
+					}
+					// fade texture
+
 					isPressed = true;
 				}
 				else if(Input.GetAxis("Horizontal") < 0){
@@ -56,11 +79,15 @@ public class MoveFlashcard : MonoBehaviour {
     {
         if (lookingAtBack == false)
         {
-            rend.material.mainTexture = cardsFront[index];
+			if (tutorialMode)
+				rend.material.mainTexture = sampleFront [index];
+            else rend.material.mainTexture = cardsFront[index];
         }
         else
         {
-            rend.material.mainTexture = cardsBack[index];
+			if (tutorialMode)
+				rend.material.mainTexture = sampleBack [index];
+            else rend.material.mainTexture = cardsBack[index];
         }
     }
 }
